dc9e9c1b61fa306d787f729f3b52cf5cbb5d023c
KatsuhitoNakashima <oct2215918@gmail.com>
2021-05-29 23:22:31 +0900

    閲覧用データに訂正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,300 +1,49 @@
 # coding: utf-8
 
-User.create!(name: "勤怠情報管理アカウント",
-             email: "spread@email.com",
+User.create!(name: "管理者",
+             email: "admin@email.com",
              password: "password",
              department: "本社",
              password_confirmation: "password",
              emp_id: "999",
              admin: true)
 
-User.create!(name: "遠藤 瑞希",
-             department: "歌志軒山王店",
-             password: "1002123",
+User.create!(name: "サンプル１",
+             email: "sample1@email.com",
+             department: "A店",
+             password: "password",
+             emp_id: "1001",
+             position: "社員",
+             password_confirmation: "password",)
+             
+User.create!(name: "サンプル２",
+             email: "sample2@email.com",
+             department: "A店",
+             password: "password",
              emp_id: "1002",
              position: "社員",
-             password_confirmation: "1002123",)
-             
-User.create!(name: "木村 亮介",
-             department: "歌志軒山王店",
-             password: "1003123",
+             password_confirmation: "password")
+
+User.create!(name: "サンプル３",
+             email: "sample3@email.com",
+             department: "B店",
+             password: "password",
              emp_id: "1003",
              position: "社員",
-             password_confirmation: "1003123")
+             password_confirmation: "password")
 
-User.create!(name: "佐橋 力斗",
-             department: "歌志軒山王店",
-             password: "1004123",
+User.create!(name: "サンプル４",
+             email: "sample4@email.com",
+             department: "C店",
+             password: "password",
              emp_id: "1004",
              position: "社員",
-             password_confirmation: "1004123")
-
-User.create!(name: "住吉 将直",
-             department: "歌志軒山王店",
-             password: "1005123",
-             emp_id: "1005",
-             position: "社員",
-             password_confirmation: "1005123")
-
-User.create!(name: "林 さつき",
-             department: "歌志軒山王店",
-             password: "1006123",
-             emp_id: "1006",
-             position: "社員",
-             password_confirmation: "1006123")
-
-User.create!(name: "松家 涼太",
-             department: "歌志軒山王店",
-             password: "1007123",
-             emp_id: "1007",
-             position: "社員",
-             password_confirmation: "1007123")
-             
-User.create!(name: "山田 亮平",
-             department: "歌志軒山王店",
-             password: "1008123",
-             emp_id: "1008",
-             position: "社員",
-             password_confirmation: "1008123")
-             
-             
-             
-User.create!(name: "有本 瑠璃",
-             department: "歌志軒山王店",
-             position: "パートナー",
-             hour_pay: 950,
-             tp_ex: 0,
-             emp_id: "9000",
-             password: "9000123",
-             password_confirmation: "9000123")
-             
-User.create!(name: "伊海 梨花",
-             department: "歌志軒山王店",
-             position: "パートナー",
-             hour_pay: 930,
-             tp_ex: 1040,
-             emp_id: "9001",
-             password: "9001123",
-             password_confirmation: "9001123")
-             
-User.create!(name: "小倉 由寛",
-             department: "歌志軒山王店",
-             position: "パートナー",
-             hour_pay: 930,
-             tp_ex: 920,
-             emp_id: "9002",
-             password: "9002123",
-             password_confirmation: "9002123")
-             
-User.create!(name: "佐藤 未来",
-             department: "歌志軒山王店",
-             position: "パートナー",
-             hour_pay: 930,
-             tp_ex: 360,
-             emp_id: "9003",
-             password: "9003123",
-             password_confirmation: "9003123")
-             
-User.create!(name: "仙台 麻妃呂",
-             department: "歌志軒山王店",
-             position: "パートナー",
-             hour_pay: 950,
-             tp_ex: 480,
-             emp_id: "9004",
-             password: "9004123",
-             password_confirmation: "9004123")
-             
-User.create!(name: "滝野 輝弥",
-             department: "歌志軒山王店",
-             position: "パートナー",
-             hour_pay: 950,
-             tp_ex: 720,
-             emp_id: "9005",
-             password: "9005123",
-             password_confirmation: "9005123")
-             
-User.create!(name: "新美 紗彩",
-             department: "歌志軒山王店",
-             position: "パートナー",
-             hour_pay: 950,
-             tp_ex: 900,
-             emp_id: "9006",
-             password: "9006123",
-             password_confirmation: "9006123")
-             
-User.create!(name: "山木田 彩恵",
-             department: "歌志軒山王店",
-             position: "パートナー",
-             hour_pay: 1000,
-             emp_id: "9007",
-             password: "9007123",
-             password_confirmation: "9007123")
-             
-             
-             
-             
-User.create!(name: "新美 まさや",
-             department: "歌志軒瑞穂店",
-             password: "1009123",
-             emp_id: "1009",
-             position: "社員",
-             password_confirmation: "1009123")
-                          
-User.create!(name: "円山 洸輝",
-             department: "歌志軒瑞穂店",
-             password: "1010123",
-             emp_id: "1010",
-             position: "社員",
-             password_confirmation: "1010123")
-                          
-                          
-User.create!(name: "朝山 珠夢",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9008",
-             password: "9008123",
-             password_confirmation: "9008123")
-                          
-User.create!(name: "飯牟禮 智仁",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9009",
-             password: "9009123",
-             password_confirmation: "9009123")
-                          
-User.create!(name: "石崎 菜々美",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9010",
-             password: "9010123",
-             password_confirmation: "9010123")
-                          
-User.create!(name: "大城 渉真",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9011",
-             password: "9011123",
-             password_confirmation: "9011123")
-                          
-User.create!(name: "小川 祐理華",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9012",
-             password: "9012123",
-             password_confirmation: "9012123")
-                          
-User.create!(name: "梶川 裕司",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9013",
-             password: "9013123",
-             password_confirmation: "9013123")
-                          
-User.create!(name: "木家 悠之介",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9014",
-             password: "9014123",
-             password_confirmation: "9014123")
-                          
-User.create!(name: "高宮 彩花",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9015",
-             password: "9015123",
-             password_confirmation: "9015123")
-                          
-User.create!(name: "所 めぐみ",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 850,
-             tp_ex: 0,
-             emp_id: "9016",
-             password: "9016123",
-             password_confirmation: "9016123")
-                          
-User.create!(name: "中川 玲奈",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 850,
-             tp_ex: 0,
-             emp_id: "9017",
-             password: "9017123",
-             password_confirmation: "9017123")
-                          
-User.create!(name: "南谷 ひより",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 850,
-             tp_ex: 0,
-             emp_id: "9018",
-             password: "9018123",
-             password_confirmation: "9018123")
-                          
-User.create!(name: "古田 拓也",
-             department: "歌志軒瑞穂店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9019",
-             password: "9019123",
-             password_confirmation: "9019123")
-             
-             
-             
-User.create!(name: "津田 愛香",
-             department: "歌志軒岐南店",
-             password: "1011123",
-             emp_id: "1011",
-             position: "社員",
-             password_confirmation: "1011123")                          
-
-User.create!(name: "長尾 清隆",
-             department: "歌志軒岐南店",
-             password: "1012123",
-             emp_id: "1012",
-             position: "社員",
-             password_confirmation: "1012123")
-                          
-User.create!(name: "中島 功人",
-             department: "歌志軒岐南店",
-             password: "password",
-             emp_id: "1013",
-             position: "社員",
              password_confirmation: "password")
-                          
-                          
-                          
-User.create!(name: "加藤 葵",
-             department: "歌志軒岐南店",
+
+User.create!(name: "サンプル５",
+             email: "sample5@email.com",
+             department: "C店",
+             password: "password",
+             emp_id: "9001",
              position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 140,
-             emp_id: "9020",
-             password: "9020123",
-             password_confirmation: "9020123")
-                                       
-User.create!(name: "三島 浩平",
-             department: "歌志軒岐南店",
-             position: "パートナー",
-             hour_pay: 900,
-             tp_ex: 0,
-             emp_id: "9021",
-             password: "9021123",
-             password_confirmation: "9021123")
+             password_confirmation: "password")
